fa4826b57a47ca4234ae54b967509cc137bd0829
simonadams <simonadams@192.168.1.111>
2018-03-16 00:31:12 +0000

    admin email

--- common/config/params.php
+++ common/config/params.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
+    'adminEmail' => 'simonjadams@gmail.com',
+    'supportEmail' => 'simonjadams@gmail.com',
     'user.passwordResetTokenExpire' => 3600,
 ];
