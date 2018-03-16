6402ea4d92072a29eeca74e9fe985dfcbb786580
simonadams <simonadams@192.168.1.111>
2018-03-16 21:07:09 +0000

    general tidy

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -10,7 +10,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
+    <p>Book appointments with therapists and clients in rooms. Some therapists can 
+        work in any room, others can only work in certain rooms due to equipment.</p>
 
-    <code><?= __FILE__ ?></code>
 </div>
