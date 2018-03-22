ddc8b7ef0854a887f36e780bfb72650023983671
simonadams <simonadams@192.168.1.111>
2018-03-22 20:58:21 +0000

    backend appointments form

--- backend/views/appointment/_form.php
+++ backend/views/appointment/_form.php
@@ -2,6 +2,11 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use common\models\Room;
+use common\models\Therapist;
+use common\models\Therapy;
+use common\models\Client;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Appointment */
@@ -11,17 +16,17 @@ use yii\widgets\ActiveForm;
 <div class="appointment-form">
 
     <?php $form = ActiveForm::begin(); ?>
+            
+    <?= $form->field($model, 'room_id')->dropdownList(ArrayHelper::map(Room::find()->all(), 'id', 'name'),['prompt'=>'Select Room']); ?>
+    
+    <?= $form->field($model, 'therapist_id')->dropdownList(ArrayHelper::map(Therapist::find()->all(), 'id', 'name'),['prompt'=>'Select Therapist']); ?>
 
-    <?= $form->field($model, 'room_id')->textInput() ?>
-
-    <?= $form->field($model, 'therapist_id')->textInput() ?>
-
-    <?= $form->field($model, 'client_id')->textInput() ?>
-
-    <?= $form->field($model, 'therapy_id')->textInput() ?>
-
+    <?= $form->field($model, 'therapy_id')->dropdownList(ArrayHelper::map(Therapy::find()->all(), 'id', 'name'),['prompt'=>'Select Therapy']); ?>
+    
+    <?= $form->field($model, 'client_id')->dropdownList(ArrayHelper::map(Client::find()->all(), 'id', 'name'),['prompt'=>'Select Client']); ?>   
+    
     <?= $form->field($model, 'timestamp')->textInput() ?>
-
+    
     <?= $form->field($model, 'minutes_duration')->textInput() ?>
 
     <div class="form-group">
