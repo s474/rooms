0b06129f995cf5dda498e39ec4cb3f3eae04ed60
simonadams <simonadams@192.168.1.111>
2018-03-26 22:16:52 +0100

    philippfrenzel/yii2fullcalendar example

--- backend/views/appointment/index.php
+++ backend/views/appointment/index.php
@@ -33,3 +33,34 @@ $this->title = 'Appointments';
         ],
     ]); ?>
 </div>
+
+<br />
+<br />
+
+<?php
+
+$events = array();
+//Testing
+$Event = new \yii2fullcalendar\models\Event();
+$Event->id = 1;
+$Event->title = 'Testing';
+$Event->start = date('Y-m-d\TH:i:s\Z');
+$event->nonstandard = [
+    'field1' => 'Something I want to be included in object #1',
+    'field2' => 'Something I want to be included in object #2',
+];
+$events[] = $Event;
+
+$Event = new \yii2fullcalendar\models\Event();
+$Event->id = 2;
+$Event->title = 'Testing';
+$Event->start = date('Y-m-d\TH:i:s\Z',strtotime('tomorrow 6am'));
+$events[] = $Event;
+
+?>
+
+<?= \yii2fullcalendar\yii2fullcalendar::widget(array(
+    'events'=> $events,
+));
+
+?>
