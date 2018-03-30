72c668c933a09b02885777e4c5111a7763248526
simonadams <simonadams@192.168.1.111>
2018-03-30 16:19:44 +0100

    yii2fullcalendar ajax, draggable events

--- backend/views/appointment/index.php
+++ backend/views/appointment/index.php
@@ -3,13 +3,70 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\helpers\Url;
+use yii\web\JsExpression;
 
 /* @var $this yii\web\View */
 /* @var $searchModel common\models\AppointmentSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
+$DragJS = <<<EOF
+/* initialize the external events
+-----------------------------------------------------------------*/
+$('#external-events .fc-event').each(function() {
+    // store data so the calendar knows to render an event upon drop
+    $(this).data('event', {
+        title: $.trim($(this).text()), // use the element's text as the event title
+        stick: true // maintain when user navigates (see docs on the renderEvent method)
+    });
+    // make the event draggable using jQuery UI
+    $(this).draggable({
+        zIndex: 999,
+        revert: true,      // will cause the event to go back to its
+        revertDuration: 0  //  original position after the drag
+    });
+});
+EOF;
+$this->registerJs($DragJS);
+
 $this->title = 'Appointments';
+
+$JSCode = <<<EOF
+function(start, end) {
+    var title = prompt('Event Title:');
+    var eventData;
+    if (title) {
+        eventData = {
+            title: title,
+            start: start,
+            end: end
+        };
+        $('#w0').fullCalendar('renderEvent', eventData, true);
+    }
+    $('#w0').fullCalendar('unselect');
+}
+EOF;
+$JSDropEvent = <<<EOF
+function(date) {
+    alert("Dropped on " + date.format());
+    if ($('#drop-remove').is(':checked')) {
+        // if so, remove the element from the "Draggable Events" list
+        $(this).remove();
+    }
+}
+EOF;
+$JSEventClick = <<<EOF
+function(calEvent, jsEvent, view) {
+    alert('Event: ' + calEvent.title);
+    alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+    alert('View: ' + view.name);
+    // change the border color just for fun
+    $(this).css('border-color', 'red');
+}
+EOF;
+    
 ?>
+
+
 <div class="appointment-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
@@ -38,33 +95,35 @@ $this->title = 'Appointments';
 <br />
 <br />
 
-<?php
-
-$events = array();
-//Testing
-$Event = new \yii2fullcalendar\models\Event();
-$Event->id = 1;
-$Event->title = 'Testing';
-$Event->start = date('Y-m-d\TH:i:s\Z');
-$event->nonstandard = [
-    'field1' => 'Something I want to be included in object #1',
-    'field2' => 'Something I want to be included in object #2',
-];
-$events[] = $Event;
-
-$Event = new \yii2fullcalendar\models\Event();
-$Event->id = 2;
-$Event->title = 'Testing';
-$Event->start = date('Y-m-d\TH:i:s\Z',strtotime('tomorrow 6am'));
-$events[] = $Event;
-
-?>
-
 <?= \yii2fullcalendar\yii2fullcalendar::widget([
-      'options' => [
-        'lang' => 'fr',
-        //... more options to be defined here!
-      ],
-      'events' => Url::to(['appointment/jsoncalendar'])
+        'clientOptions' => [
+            'selectable' => true,
+            'selectHelper' => true,
+            'droppable' => true,
+            'editable' => true,
+            'drop' => new JsExpression($JSDropEvent),
+            'select' => new JsExpression($JSCode),
+            'eventClick' => new JsExpression($JSEventClick),
+            'defaultDate' => date('Y-m-d')
+        ],
+        //'ajaxEvents' => Url::toRoute(['/site/jsoncalendar']),
+        'options' => [
+            'lang' => 'fr',
+            //... more options to be defined here!
+        ],
+        'ajaxEvents' => Url::to(['appointment/jsoncalendar'])
     ]);
 ?>
+
+<div id="external-events">
+    <h4>Draggable Events</h4>
+    <div class="fc-event ui-draggable ui-draggable-handle">My Event 1</div>
+    <div class="fc-event ui-draggable ui-draggable-handle">My Event 2</div>
+    <div class="fc-event ui-draggable ui-draggable-handle">My Event 3</div>
+    <div class="fc-event ui-draggable ui-draggable-handle">My Event 4</div>
+    <div class="fc-event ui-draggable ui-draggable-handle">My Event 5</div>
+    <p>
+        <input type="checkbox" id="drop-remove">
+        <label for="drop-remove">remove after drop</label>
+    </p>
+</div>
