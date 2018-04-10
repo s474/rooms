8e66d0a5dd4283abd2fb3f53cbfd12c8bef13ce8
simonadams <simonadams@192.168.1.111>
2018-04-10 21:57:59 +0100

    tidy

--- common/models/Appointment.php
+++ common/models/Appointment.php
@@ -71,7 +71,10 @@ class Appointment extends \yii\db\ActiveRecord
             ->orWhere(['and', ['>=', 'start', $this->start], ['<', 'start', $end], ['>', 'end', $end]]); // 4 
                                                 
         // And if Room, Therapist or Client are the same
-        $appts->andWhere(['or', ['=','room_id',$this->room_id], ['=', 'therapist_id', $this->therapist_id], ['=', 'client_id', $this->client_id]]);
+        $appts->andWhere(['or', 
+            ['=','room_id',$this->room_id], 
+            ['=', 'therapist_id', $this->therapist_id], 
+            ['=', 'client_id', $this->client_id]]);
                 
         if (!$this->isNewRecord)
             $appts->andWhere(['!=', 'id', $this->id]);                
