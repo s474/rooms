c8735be8732f9c8271c3f375571272c7d8e47aeb
simonadams <simonadams@192.168.1.111>
2018-04-10 20:46:48 +0100

    Appointments default sort order by start

--- common/models/AppointmentSearch.php
+++ common/models/AppointmentSearch.php
@@ -47,6 +47,7 @@ class AppointmentSearch extends Appointment
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'sort' => ['defaultOrder' => ['start'=>SORT_DESC]]
         ]);
 
         $this->load($params);
