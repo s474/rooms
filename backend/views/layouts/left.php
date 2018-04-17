360d5d5482dc31f4d6903c1f51ef22f3196d822a
simonadams <simonadams@192.168.1.111>
2018-04-17 21:57:15 +0100

    theme

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -3,6 +3,7 @@
     <section class="sidebar">
 
         <!-- Sidebar user panel -->
+        <!--
         <div class="user-panel">
             <div class="pull-left image">
                 <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
@@ -13,7 +14,8 @@
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
         </div>
-
+        -->
+        
         <!-- search form -->
         <form action="#" method="get" class="sidebar-form">
             <div class="input-group">
@@ -30,7 +32,7 @@
             [
                 'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
                 'items' => [
-                    ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
+                    //['label' => 'Menu Yii2', 'options' => ['class' => 'header']],                    
                     ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
                     ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
                     ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
