e2762323220ca1e464e78f4154cf1d910043892a
Simon Adams <simonjadams@gmail.com>
2018-06-24 19:03:56 +0100

    create appointment rooms drop down join location

--- common/components/RoomsAR.php
+++ common/components/RoomsAR.php
@@ -6,12 +6,17 @@ use Yii;
 class RoomsAR extends \yii\db\ActiveRecord
 {	
 
-    public static function find($skipCompany = false)
+    public static function find($skipCompany = false, $join = false)
     {
         if ($skipCompany) {
             return parent::find();
-        } else {            
-            return parent::find()->andFilterWhere(['company_id' => Yii::$app->session->get('company_id')]);
+        } else {    
+            $t = '';
+            if ($join)
+                $t = $join . '.company_id';
+            else 
+                $t = 'company_id';
+            return parent::find()->andFilterWhere([$t => Yii::$app->session->get('company_id')]);
         }
     }         
 
